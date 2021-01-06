46c1ae829873f3c35e8d2fd0bd2ae40d120a0fa5
Victor Lopez <vjlopezcarrillo@gmail.com>
2021-01-06 16:30:55 -0700

    Python3

--- nightly/git.sh
+++ nightly/git.sh
@@ -39,7 +39,24 @@ clonepython3(){
     fi
 }
 
+clonepython3_lowercase(){
+    if [ -d "$BASE/$2/.git" ]; then
+        pushd "$BASE/$2"
+        git checkout python3
+        git reset --hard
+        git pull
+        popd
+    else
+        if [ -e "$REFS/$2/.git" ]; then
+            git clone -b Python3 $1 "$BASE/$2" --reference "$REFS/$2"
+        else
+            git clone -b Python3 $1 "$BASE/$2"
+        fi
+    fi
+}
+
+
 updatepython3 https://github.com/greentwip/renpy-build
-clonepython3 https://github.com/Greentwip/renpy renpy
+clonepython3_lowercase https://github.com/Greentwip/renpy renpy
 clonepython3 https://github.com/greentwip/pygame_sdl2 pygame_sdl2
 #update https://github.com/renpy/renpyweb renpyweb
