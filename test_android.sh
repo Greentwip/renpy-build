b270e73c51e5f9696cd7b5596ad4439a12a54159
Victor Lopez <vjlopezcarrillo@gmail.com>
2021-01-06 17:32:36 -0700

    Build support for Python 3

--- test_android.sh
+++ test_android.sh
@@ -3,14 +3,25 @@
 set -e
 
 ROOT="$(dirname $(readlink -f $0))"
-PYTHON_VERSION="$4"
-export RENPY_PYTHON_VER="$PYTHON_VERSION"
+PYTHON_VERSION="3"
+export RENPY_PYTHON_VER=$PYTHON_VERSION
 
-pushd "$ROOT"
-if [ "$2" == "build" ]; then
-    ./build.py --platform android rebuild rapt rapt-sdl2
+
+if [ "$1" == "build" ]; then
+    pushd "$ROOT"
+        ./build.py --platform android rebuild rapt rapt-sdl2
+    popd    
+elif [ "$1" == "install-sdk" ]; then
+    pushd "$ROOT/renpy/rapt2"
+        export PGS4A_NO_TERMS=1
+	    python android.py installsdk
+    popd
+elif [ "$1" != "" ]; then
+    pushd "$ROOT/renpy/rapt2"
+        ./android.py build "$2" installDebug --launch
+    popd
 fi
-popd
+
 
 
 #rm -f "$ROOT/renpy/rapt/Sdk"
@@ -18,16 +29,8 @@ popd
 #mkdir -p "$ROOT/renpy/rapt/project"
 #cp -a /home/tom/ab/android/local.properties "$ROOT/renpy/rapt/project"
 
-pushd "$ROOT/renpy/rapt2"
-if [ "$3" == "install-sdk" ]; then
-    export PGS4A_NO_TERMS=1
-	python android.py installsdk
-fi
-popd
+
+
 
 #rm -Rf "$ROOT/renpy/rapt/prototype/renpyandroid/src/main/java" || true
 #cp -aL rapt/prototype/renpyandroid/src/main/java $ROOT/renpy/rapt/prototype/renpyandroid/src/main/
-
-if [ "$1" != "" ]; then
-    $ROOT/renpy/renpy.sh $ROOT/renpy/launcher android_build "$1" installDebug --launch
-fi
