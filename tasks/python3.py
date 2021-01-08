100fe3bef9bbc0f1ce4d20f3bd84e30b5655491c
root <root@DESKTOP-F8C2TSG.localdomain>
2021-01-08 02:26:13 -0700

    Hope we don't need all those error modules

--- tasks/python3.py
+++ tasks/python3.py
@@ -111,14 +111,26 @@ def build_android(c):
 
     c.env("CONFIG_SITE", "config.site")
 
-    c.env("CFLAGS", "{{ CFLAGS }} -DXML_POOR_ENTROPY=1 -DUSE_PYEXPAT_CAPI -DHAVE_EXPAT_CONFIG_H -DOPENSSL_THREADS")
+    c.env("CFLAGS", "-Wimplicit-function-declaration ")
+
+    c.env("CFLAGS", "{{ CFLAGS }} -DXML_POOR_ENTROPY=1 ")
+    c.env("CFLAGS", "{{ CFLAGS }} -DUSE_PYEXPAT_CAPI ")
+    c.env("CFLAGS", "{{ CFLAGS }} -DHAVE_EXPAT_CONFIG_H")
+    c.env("CFLAGS", "{{ CFLAGS }} -DOPENSSL_THREADS ")
+    
+    c.env("LDFLAGS", "{{ LDFLAGS }}")
+
+    c.env("LDFLAGS", "{{LDFLAGS}} -shared --verbose ")        
+    c.env("LDFLAGS", "{{ LDFLAGS}} -pthread")
+
 
     c.env("READELF", "arm-linux-androideabi-readelf")
-    #c.run("""./configure --target=arm-linux-androideabi --build=x86_64-linux-gnu  {{ cross_config }} --prefix="{{ install }}" --with-system-ffi --enable-ipv6""")
-    #c.run("""./configure  {{ cross_config }} --prefix="{{ install }}" --with-system-ffi --enable-ipv6""")
+    c.run("""./configure --target=arm-linux-androideabi --build=x86_64-linux-gnu  {{ cross_config }} --prefix="{{ install }}" --with-system-ffi --enable-ipv6""")
 
     c.generate("{{ source }}/Python-{{ version }}-Setup.local", "Modules/Setup")
 
+    c.run("""{{ make }}""")
+    
     c.run("""{{ make }} install""")
 
     c.copy("{{ host }}/bin/python3", "{{ install }}/bin/hostpython3")
