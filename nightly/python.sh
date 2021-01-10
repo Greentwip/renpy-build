7d72e67635c853ffcb9d250b1d632bbb0d8fb65c
Victor Lopez <vjlopezcarrillo@gmail.com>
2021-01-09 21:37:29 -0700

    Prepare does not need all that

--- nightly/python.sh
+++ nightly/python.sh
@@ -11,20 +11,6 @@ fi
 pip install --upgrade pip
 pip install -r $ROOT/requirements.txt
 
-pushd $BASE/pygame_sdl2
-#python ./fix_virtualenv.py || true
-popd
-
-pushd $BASE/pygame_sdl2
-python setup.py install_headers
-popd
-
-pushd $BASE/renpy
-
-if [ ! -e pygame_sdl2 ]; then
-    ln -s $BASE/pygame_sdl2 .
-fi
-
 # Delete the generated files.
 rm -Rf renpy/module/gen-static
 rm -Rf renpy/module/gen3-static
