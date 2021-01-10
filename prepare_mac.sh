c3d0519c659863c270d95e1855b695396fece12a
Victor Lopez <vjlopezcarrillo@gmail.com>
2021-01-09 21:46:30 -0700

    Separate git from python commands

--- prepare_mac.sh
+++ prepare_mac.sh
@@ -7,34 +7,6 @@ ROOT=$(cd $(dirname $0); pwd)
 REFS=$ROOT
 BASE="$ROOT"
 
-# Needed to build things.
-brew install ccache
-
-# Needed by renpy-build itself.
-brew install jinja2-cli
-
-# Needed by gcc.
-brew install gmp mpfr mpc
-
-# Needed by hostpython.
-brew install openssl bzip2
-
-# Needed for mac
-brew install cmake xml2
-
-# Needed for web
-brew install quilt
-
-# Needed for pyenv
-brew install zlib
-
-# Needed for some build scripts
-brew install coreutils
-
-
-# Install the standard set of packages needed to build Ren'Py.
-brew install ffmpeg freetype fribidi glew sdl2 sdl2_image sdl2_gfx sdl2_mixer sdl2_ttf jpeg-turbo
-
 mkdir -p "$BASE/tmp"
 
 
