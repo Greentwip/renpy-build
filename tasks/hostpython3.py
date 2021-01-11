63b97f227efd23b124c9e81710d0ec3540c50266
Victor Lopez <vjlopezcarrillo@gmail.com>
2021-01-10 22:37:45 -0700

    Finish building python

--- tasks/hostpython3.py
+++ tasks/hostpython3.py
@@ -26,7 +26,8 @@ def build_host(c):
     c.env("CONFIG_SITE", "config.site")
 
     c.env("CFLAGS", "-DXML_POOR_ENTROPY=1 -DUSE_PYEXPAT_CAPI -DHAVE_EXPAT_CONFIG_H ")
-    c.env("LDFLAGS", "")
+    c.env("CFLAGS", "{{CFLAGS}} -I/usr/local/opt/openssl/include")
+    c.env("LDFLAGS", " -L/usr/local/opt/openssl/lib -l:ssl.a -l:crypto.a")
     c.run("""./configure --prefix="{{ host }}" --enable-ipv6""")
 
     c.generate("{{ source }}/Python-{{ version }}-Setup.local", "Modules/Setup")
