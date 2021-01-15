568e59b03b5c93211c0cb52ae3bffa9105227e44
Victor Lopez <vjlopezcarrillo@gmail.com>
2021-01-14 20:57:11 -0700

    Building static objects

--- tasks/pygame_sdl2.py
+++ tasks/pygame_sdl2.py
@@ -12,8 +12,8 @@ def gen_static(c):
     c.chdir("{{ pygame_sdl2 }}")
     c.env("PYTHONPATH", "{{host}}/lib/{{pythonver}}/lib-dynload")
     c.env("_PYTHON_SYSCONFIGDATA_NAME", "_sysconfigdata__linux_")
-    c.env("CFLAGS", "{{CFLAGS}} -I{{sysroot_include}} -I{{sysroot_lib}} -I{{install}}/include")
-    c.env("LDFLAGS", "{{LDFLAGS}} -L{{sysroot_lib}} -L{{install}} -L{{install}}/lib")
+    c.env("CFLAGS", "{{CFLAGS}} -I{{sysroot_include}} -I{{sysroot_lib}} -I{{install}}/include ")
+    c.env("LDFLAGS", "{{LDFLAGS}} -L{{sysroot_lib}} -L{{install}} -L{{install}}/lib ")
     c.env("PYGAME_SDL2_STATIC", "True")
     os.environ['_PYTHON_SYSCONFIGDATA_NAME'] = '_sysconfigdata__linux_'
     c.run("{{ host }}/bin/python3 setup.py generate")
